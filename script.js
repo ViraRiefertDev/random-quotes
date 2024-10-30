5395c5e9acc3b28c914595ed606590e0140957d7
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-10-30 19:09:00 +0100

    Selecting quote from the array of quotes

---
+++ script.js
@@ -0,0 +1,17 @@
+const quotes = [
+   'The only way to do great work is to love what you do. - Steve Jobs',
+   'Innovation distinguishes between a leader and a follower. - Steve Jobs',
+   "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
+];
+
+const quoteElement = document.getElementById('quote');
+const generateBtn = document.getElementById('generate-btn');
+
+function generateRandomQuote() {
+   const randomIndex = Math.floor(Math.random() * quotes.length);
+   const randomQuote = quotes[randomIndex];
+}
+
+generateBtn.addEventListener('click', generateRandomQuote);
+//Generate a random quote when the page loads
+/* generateRandomQuote();*/
